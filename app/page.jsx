a5f164b0b172069aff609e5e010259f3e9a3bc8e
JJatau <jjatau14@outlook.com>
2023-12-02 00:13:25 +0000

    added window object checks

--- app/page.jsx
+++ app/page.jsx
@@ -18,12 +18,14 @@ const HomePage = () => {
     let screenPosition = [0, -6.5, -43];
     let rotation = [0.1, 4.7, 0];
 
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-      screenPosition = [0, -6.5, -43];
-    } else {
-      screenScale = [1, 1, 1];
-      screenPosition = [0, -6.5, -43];
+    if (typeof window !== 'undefined') {
+      if (window.innerWidth < 768) {
+        screenScale = [0.9, 0.9, 0.9];
+        screenPosition = [0, -6.5, -43];
+      } else {
+        screenScale = [1, 1, 1];
+        screenPosition = [0, -6.5, -43];
+      }
     }
 
     return [screenScale, screenPosition, rotation];
@@ -31,13 +33,15 @@ const HomePage = () => {
 
   const adjustPlaneForScreenSize = () => {
     let screenScale, screenPosition;
-
-    if (window.innerWidth < 768) {
-      screenScale = [1.5, 1.5, 1.5];
-      screenPosition = [0, -1.5, 0];
-    } else {
-      screenScale = [3, 3, 3];
-      screenPosition = [0, -4, -4];
+    
+    if (typeof window !== 'undefined') {
+      if (window.innerWidth < 768) {
+        screenScale = [1.5, 1.5, 1.5];
+        screenPosition = [0, -1.5, 0];
+      } else {
+        screenScale = [3, 3, 3];
+        screenPosition = [0, -4, -4];
+      }
     }
 
     return [screenScale, screenPosition];
